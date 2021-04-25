36850440017d335757acae3d803b5e0c5d5175ed
glongh <glongh@gmail.com>
2021-04-25 17:47:16 -0400

    feat(*): add representation of seconds in human readable periods

---
+++ ratelimiter/time.py
@@ -0,0 +1,11 @@
+class Time:
+    """
+      Representation of seconds in human readable periods 
+    """
+    SECOND = 1
+    MINUTE = 60
+    HOUR = 3600
+    DAY = 24 * 3600
+    WEEK = 7 * 24 * 3600
+    MONTH = 30 * 24 * 3600
+    YEAR = 365 * 24 * 3600
