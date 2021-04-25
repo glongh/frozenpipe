429e1ad6b6a439d261c1882277a171d423bd795d
glongh <glongh@gmail.com>
2021-04-25 17:44:10 -0400

    test(*): Add tests for refilling bucket

--- test/test_bucket.py
+++ test/test_bucket.py
@@ -1,21 +1,7 @@
-from ratelimiter.bucket import Bucket
+from ratelimiter import Bucket
 import time
 import pytest
-
-
-class FakeTimer():
-    """
-    A fake timer to test against time
-    """
-
-    def __init__(self):
-        self._timestamp = 0.0
-
-    def tick(self, seconds):
-        self._timestamp += seconds
-
-    def time(self):
-        return self._timestamp
+from test_tools import FakeTimer
 
 
 @pytest.fixture
